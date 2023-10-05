122d1424237094c81b109fb430bb615c21d18cb2
happidence1 <45323591+happidence1@users.noreply.github.com>
2023-10-05 11:39:55 -0500

    Add files via upload

---
+++ job-submit.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+##NECESSARY JOB SPECIFICATIONS
+#SBATCH --job-name=pytorch-gpu       #Set the job name to "pytorch-gpu"
+#SBATCH --time=01:00:00              #Set the wall clock limit to 1hr
+#SBATCH --nodes=1                    #Request 1 node
+#SBATCH --ntasks-per-node=1          #Request 1 task per node
+#SBATCH --cpus-per-task=8           #Request 16 cpus per task
+#SBATCH --partition=gpu              #Specify partition as GPU if required to run the workload on GPU
+#SBATCH --gres=gpu:a30:1             #Specify GPU architecture and number of GPU required
+#SBATCH --mem=40G                   #Request 240G per node
+#SBATCH --output=pytorch-gpu.%j      #Send stdout/err to "pytorch-gpu.[jobID]"
+##SBATCH --nodelist=fc040
+##SBATCH --exclusive
+##SBATCH --reservation=shortcourse
+
+ml purge
+ml WebProxy
+ml Anaconda3/2022.05
+source activate mylab
+python main.py
